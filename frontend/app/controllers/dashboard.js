cb65b9a073fab99e6f3a757abaf9939103d1f644
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2025-02-20 11:23:40 +0100

    cleanup

--- frontend/app/controllers/dashboard.js
+++ frontend/app/controllers/dashboard.js
@@ -4,7 +4,7 @@ import { tracked } from '@glimmer/tracking';
 
 export default class DashboardController extends Controller {
   @tracked isSentFikaListVisible = false;
-  @tracked isReceivedFikaListVisible = true;
+  @tracked isReceivedFikaListVisible = false;
 
   @action
   toggleSentFikaList() {
